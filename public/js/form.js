6657dc4d906f5d5fbc4a4d9fcf034874b6b743b8
RobertC91 <134195467+RobertC91@users.noreply.github.com>
2023-09-13 19:36:58 -0500

    edited file structure and added js form handlers

---
+++ public/js/form.js
@@ -0,0 +1,11 @@
+var fieldHTMLAcc =
+      '<div class="input-group"><select name="account_type[]">\
+    <option value="account-type">Expense Type</option>\
+    <option value="checking">Car & Gas</option>\
+    <option value="savings">Grocery</option>\
+    <option value="credit-card">Restaurant</option>\
+    <option value="auto-loan">Subscriptions</option>\
+    <option value="other">Other</option>\
+    </select><input type="text" name="field_name[]" placeholder="Expense Name" value=""/>\
+    <input type="number" name="field_name[]" placeholder="Amount" value=""/>\
+    <a href="javascript:void(0);" class="remove_button">Delete</a></div>';
