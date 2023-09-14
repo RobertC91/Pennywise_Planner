8a404ce834dcd725e0263cb66aef3c91b881a617
RobertC91 <134195467+RobertC91@users.noreply.github.com>
2023-09-13 21:32:34 -0500

    Heroku deployment 7

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const SequelizeStore = require("connect-session-sequelize")(session.Store);
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001;
 
 const hbs = exphbs.create({});
 
